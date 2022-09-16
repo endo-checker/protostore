424f11755b727a37e9315df2393ba1cda566ba20
Your Name <you@example.com>
2022-09-16 15:10:18 +1200

    initial commit

---
+++ server/server.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
+)
+
+func HttpGrpcMux(httpHandler http.Handler, grpcServer *grpc.Server) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			if allowedOrigin(r.Header.Get("Origin")) {
+				w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+				w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+				w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization, ResponseType")
+			}
+			if r.Method == "OPTIONS" {
+				return
+			}
+			httpHandler.ServeHTTP(w, r)
+		}
+	})
+}
+
+func allowedOrigin(origin string) bool {
+	if viper.GetString("cors") == "*" {
+		return true
+	}
+	if matched, _ := regexp.MatchString(viper.GetString("cors"), origin); matched {
+		return true
+	}
+	return false
+}
