0545b1e91df3f1624f2fc27e6e28d0e772c87f6d
Your Name <you@example.com>
2022-09-21 15:16:08 +1200

    fixed

--- go.mod
+++ go.mod
@@ -3,11 +3,8 @@ module github.com/endo-checker/common
 go 1.19
 
 require (
-	github.com/dapr/dapr v1.8.4
 	github.com/joho/godotenv v1.4.0
 	github.com/spf13/viper v1.13.0
-	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e
-	google.golang.org/protobuf v1.28.1
 )
 
 require (
@@ -16,18 +13,20 @@ require (
 	github.com/klauspost/compress v1.14.4 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	google.golang.org/genproto v0.0.0-20220822174746-9e6da59bd2fc // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.11.3
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.5.1-0.20220423185008-bf980b35cac4 // indirect
--- server/server.go
+++ server/server.go
@@ -1,131 +1,13 @@
 package server
 
 import (
-	"context"
-	"log"
 	"net/http"
-	"os"
 	"regexp"
 	"strings"
 
-	daprpb "github.com/dapr/dapr/pkg/proto/runtime/v1"
-	gw "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/spf13/viper"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/reflection"
-	"google.golang.org/protobuf/encoding/protojson"
 )
-
-const defPort = "8080"
-
-// Handler is implemented by a service's grpc 'handler' to register with the
-// server and grpc-gateway mux
-type Server interface {
-	// RegisterGRPC registers a service's handler (server) with the gRPC server.
-	RegisterGRPC(srv *grpc.Server, h interface{})
-	// RegisterHTTP registers http servers for a service using a grpc-gateway
-	// generated function.
-	RegisterHTTP(ctx context.Context, mux *gw.ServeMux, endpoint string, opts []grpc.DialOption) error
-}
-
-// Server type is used by Server receiver methods
-type server struct {
-	port    string
-	servers []Server
-}
-
-// ServerOption is used for startup options passed on initialisation.
-type ServerOption func(*server)
-
-// NewServer creates a new Server instance
-func NewServer(opts ...ServerOption) *server {
-	// check to see if Dapr APP_PORT has been set
-	port := os.Getenv("APP_PORT")
-	if port == "" {
-		port = defPort
-	}
-
-	// apply options
-	s := server{port: port}
-	for _, opt := range opts {
-		opt(&s)
-	}
-	return &s
-}
-
-func (s *server) Serve() error {
-	addr := ":" + s.port
-
-	grpcSrv := grpc.NewServer()
-	defer grpcSrv.Stop()
-	reflection.Register(grpcSrv)
-
-	dopts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
-
-	// map HTTP headers to gRPC context
-	hm := gw.WithIncomingHeaderMatcher(func(key string) (string, bool) {
-		switch key {
-		case "X-Token-C-Tenant", "X-Token-C-User", "Permissions":
-			return key, true
-		default:
-			return gw.DefaultHeaderMatcher(key)
-		}
-	})
-
-	// JSON marshaling/unmarshaling
-	mo := gw.WithMarshalerOption("*", &gw.JSONPb{
-		MarshalOptions: protojson.MarshalOptions{
-			EmitUnpopulated: false,
-		},
-		// UnmarshalOptions: protojson.UnmarshalOptions{
-		// 	DiscardUnknown: false,
-		// },
-	})
-
-	// multiplex gRPC and HTTP on same port
-	httpMux := gw.NewServeMux(hm, mo)
-	mux := httpGrpcMux(httpMux, grpcSrv)
-	httpSrv := &http.Server{
-		Addr:    addr,
-		Handler: h2c.NewHandler(mux, &http2.Server{}),
-	}
-
-	// register grpc servers
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	for _, srv := range s.servers {
-		srv.RegisterGRPC(grpcSrv, srv)
-		srv.RegisterHTTP(ctx, httpMux, addr, dopts)
-
-		// register Dapr server
-		if cb, ok := srv.(daprpb.AppCallbackServer); ok {
-			daprpb.RegisterAppCallbackServer(grpcSrv, cb)
-		}
-	}
-
-	log.Println("service starting  on", addr)
-
-	if err := httpSrv.ListenAndServe(); err != http.ErrServerClosed {
-		return err
-	}
-
-	return nil
-}
-
-func WithAppPort(port string) ServerOption {
-	return func(s *server) {
-		s.port = port
-	}
-}
-
-func WithServer(srv Server) ServerOption {
-	return func(s *server) {
-		s.servers = append(s.servers, srv)
-	}
-}
 
 func allowedOrigin(origin string) bool {
 	if viper.GetString("cors") == "*" {
@@ -137,7 +19,7 @@ func allowedOrigin(origin string) bool {
 	return false
 }
 
-func httpGrpcMux(httpHandler http.Handler, grpcServer *grpc.Server) http.Handler {
+func HttpGrpcMux(httpHandler http.Handler, grpcServer *grpc.Server) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
 			grpcServer.ServeHTTP(w, r)
