9ea321cfcb45b18679716bef17fa6fdc97a92c10
el-zacharoo <you@example.com>
2023-03-17 12:29:54 +1300

    create new db var [tag:patch]

--- store/cnn.go
+++ store/cnn.go
@@ -11,29 +11,28 @@ import (
 )
 
 type Store[T proto.Message] struct {
-	protoField string
-	locaColl   *mongo.Collection
+	locaColl *mongo.Collection
+	Fetcher  func() T
 }
 
 // add your mongo uri, and collection name
 // connect to your proto.Message type
 // e.g. store.Connect[*proto.Message]("mongodb://localhost:27017", "info")
-func Connect[T proto.Message](uri, coll string) Store[T] {
+func Connect[T proto.Message](uri string) *Store[T] {
 	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	db := client.Database("info")
-	db.Collection(coll)
-
 	msg := *new(T)
 	pbName := string(msg.ProtoReflect().Descriptor().Name())
 	pbName = strings.ToLower(pbName)
 
-	return Store[T]{
-		locaColl:   db.Collection(coll),
-		protoField: pbName,
+	db := client.Database("info")
+
+	return &Store[T]{
+		locaColl: db.Collection(pbName),
+		Fetcher:  func() T { return *new(T) },
 	}
 }
