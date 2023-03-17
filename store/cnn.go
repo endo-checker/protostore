7b362b0efa21816d879e14b52e5f92f2ca2b9c72
el-zacharoo <you@example.com>
2023-03-17 16:37:56 +1300

    added cli opts p[tag:patch]

--- store/cnn.go
+++ store/cnn.go
@@ -18,7 +18,10 @@ type Store[T proto.Message] struct {
 // add your mongo uri, and collection name
 // connect to your proto.Message type
 // e.g. store.Connect[*proto.Message]("mongodb://localhost:27017", "info")
-func Connect[T proto.Message](uri string) Store[T] {
+func Connect[T proto.Message](uri string, opts ...ClientOption) error {
+
+	var err error
+
 	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
@@ -32,44 +35,5 @@ func Connect[T proto.Message](uri string) Store[T] {
 	db := client.Database("info")
 	db.Collection(pbName)
 
-	return Store[T]{
-		locaColl:   db.Collection(pbName),
-		protoField: pbName,
-	}
-}
-
-// clientOpts is used to pass data the ClientOption function
-type clientOpts struct {
-	app string
-
-	db *mongo.Database
-}
-
-// ClientOption is used to pass optional arguments when creating a Client.
-type ClientOption func(*clientOpts) error
-
-// App is the name of the Mongo App to connect to .
-func WithApp(a string) ClientOption {
-	return func(c *clientOpts) error {
-		c.app = a
-		return nil
-	}
-}
-
-// set reference to Mongo database
-func (s *Store[T]) setDB(db *mongo.Database) error {
-
-	// apply configuration (indexes, etc.)
-
-	s.locaColl = db.Collection(s.protoField)
-
-	// track data changes for auditing purposes
 	return nil
-}
-
-// WithStore adds a Store reference to the client.
-func WithStore[T proto.Message](s *Store[T]) ClientOption {
-	return func(c *clientOpts) error {
-		return s.setDB(c.db)
-	}
 }
