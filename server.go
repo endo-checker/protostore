edbe851a2b37e6b824e39c511e2a306988016454
el-zacharoo <you@example.com>
2023-04-04 16:06:53 +1200

    [tag:patch] new cors

--- server.go
+++ server.go
@@ -1,7 +1,12 @@
-package server 
+package server
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/rs/cors"
 	"golang.org/x/net/http2"
@@ -9,30 +14,41 @@ import (
 )
 
 func setCORS() *cors.Cors {
-	c := cors.New(cors.Options{
+	return cors.New(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedHeaders:   []string{"Access-Control-Allow-Origin", "Content-Type"},
 		AllowedMethods:   []string{"POST", "OPTIONS"},
 		AllowCredentials: true,
 	})
-	return c
-}
-
-// connect handler setup, need to initiate http.ServeMux as well as variables
-func (s Server) ConnectServer(path string, h http.Handler, port string) {
-	c := setCORS()
-
-	mux := http.NewServeMux()
-
-	mux.Handle(path, h)
-	handler := c.Handler(mux)
-
-	http.ListenAndServe(
-		port,
-		h2c.NewHandler(handler, &http2.Server{}),
-	)
 }
 
 type Server struct {
 	*http.ServeMux
+}
+
+func (s *Server) ConnectServer(path string, h http.Handler, port string) {
+	c := setCORS()
+	s.ServeMux = http.NewServeMux()
+	s.Handle(path, c.Handler(h))
+
+	// Start the server
+	srv := &http.Server{
+		Addr:    port,
+		Handler: h2c.NewHandler(s, &http2.Server{}),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("shutting down server...")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Fatalf("failed to gracefully shutdown server: %v", err)
+	}
 }
