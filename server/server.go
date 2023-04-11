35c192b65579df83de03954dcec208d588fd0030
el-zacharoo <you@example.com>
2023-04-11 14:41:55 +1200

    changes [tag:patch]

--- server/server.go
+++ server/server.go
@@ -1,16 +1,20 @@
 package server
 
 import (
+	"context"
 	"fmt"
-	"log"
+
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
-// Server represents a HTTP server with CORS support
 type Server struct {
 	*http.ServeMux
 }
@@ -26,27 +30,55 @@ func newCorsHandler() (*cors.Cors, error) {
 	}), nil
 }
 
-// listenAndServe starts the HTTP server and listens for incoming requests
-func (s *Server) listenAndServe(port string) error {
-	log.Printf("Server listening on port %s\n", port)
-	err := http.ListenAndServe(port, h2c.NewHandler(s, &http2.Server{}))
-	if err != nil && err != http.ErrServerClosed {
-		return fmt.Errorf("failed to start server on port %s: %v", port, err)
+func listenServe(srv *http.Server) error {
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("failed to start server: %v", err)
 	}
 	return nil
 }
 
-// ConnectServer connects a HTTP handler to a server and starts listening for requests
-func (s *Server) ConnectServer(path string, h http.Handler, port string) error {
+func (s *Server) ConnectServer(ctx context.Context, path string, h http.Handler, port string) error {
 	c, err := newCorsHandler()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to set CORS: %v", err)
 	}
-
 	s.ServeMux = http.NewServeMux()
 	s.Handle(path, c.Handler(h))
 
-	go s.listenAndServe(port)
+	// Start the server
+	srv := &http.Server{
+		Addr:    port,
+		Handler: h2c.NewHandler(s, &http2.Server{}),
+	}
+
+	go listenServe(srv)
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for a signal
+	select {
+	case <-ctx.Done():
+		fmt.Println("shutting down server...")
+		// Give the server 5 seconds to gracefully shutdown
+		ctxShutdown, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctxShutdown); err != nil {
+			return fmt.Errorf("failed to gracefully shutdown server: %v", err)
+		}
+
+	case sig := <-quit:
+		fmt.Printf("received signal %s", sig)
+		// Give the server 5 seconds to gracefully shutdown
+		ctxShutdown, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctxShutdown); err != nil {
+			return fmt.Errorf("failed to gracefully shutdown server: %v", err)
+		}
+	}
 
 	return nil
 }
